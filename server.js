d74b03d8f452fe451e23e112bbff90c9f9dd460b
Andrew Fielden <andrew.fielden@ahead4.biz>
2016-09-26 11:44:07 +0100

    Update port for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
 
@@ -31,4 +32,4 @@ app.use(express.static(__dirname + '/public'));
 
 //console.log(__dirname);
 
-app.listen(3000);
+app.listen(PORT);
